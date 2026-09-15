2d7cabc1af2978692d8ac09e33fca8284612637f
sachinadat <sachinadat@gmail.com>
2026-09-15 10:33:03 -0400

    Remove unwanted comments

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
+
 router.get('/', async (req, res) => {
     try {
-        // Task 1: Connect to MongoDB and store connection to db constant
         const db = await connectToDatabase();
-
-        // Task 2: use the collection() method to retrieve the gift collection
         const collection = db.collection("gifts");
-
-        // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
         const gifts = await collection.find({}).toArray();
-
-        // Task 4: return the gifts using the res.json method
         res.json(gifts);
     } catch (e) {
         console.error('Error fetching gifts:', e);
@@ -22,15 +16,9 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        // Task 1: Connect to MongoDB and store connection to db constant
         const db = await connectToDatabase();
-
-        // Task 2: use the collection() method to retrieve the gift collection
         const collection = db.collection("gifts");
-
         const id = req.params.id;
-
-        // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
         const gift = await collection.findOne({ id: id });
 
         if (!gift) {
