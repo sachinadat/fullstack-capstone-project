8f9a85d09857f70af5ea4dea519487cf38d689e5
Sachin Adat <sachinadat@gmail.com>
2026-09-17 18:16:08 -0400

    Fix linting & compile errors

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { urlConfig } from '../../config';
 import { useAppContext } from '../../context/AuthContext';
